cc27bd11fe05c03010f5a1c25c027356fe2fbac1
Eric Ko <ericko2000@hotmail.com>
2020-06-17 23:50:28 +0800

    2020/06/17 The first commit at local. add Eric_Frame_01.java

---
+++ Eric_Frame_01.java
@@ -0,0 +1,19 @@
+import java.awt.Frame;
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.Image;
+
+public class Eric_Frame_01 extends Frame
+{
+	public static void main(String[] args)
+	{
+		Eric_Frame_01 frm = new Eric_Frame_01();
+		Image img = Toolkit.getDefaultToolkit().getImage("river.jpg");
+		frm.setIconImage(img);
+		Color clr = new Color(130, 170, 200);
+		frm.setBackground(clr);
+		frm.setSize(800, 600);
+		frm.setLocation(100, 100);
+		frm.setVisible(true);
+	}
+}
